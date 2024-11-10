e6ae2ab13f9d4cb1c791be0e6c1a5d895f908641
Aritra Chatterjee <ach142818@gmail.com>
2024-11-10 23:41:51 +0530

    factorials

---
+++ src/Practice/Factorials.java
@@ -0,0 +1,24 @@
+package Practice;
+
+import java.util.Scanner;
+
+public class Factorials {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+
+        System.out.println(factorial(num));
+    }
+
+    private static long factorial(int num) {
+        long ans = 1;
+
+        for (int i = 1; i<=num; i++) {
+            ans = ans * i;
+        }
+
+        return ans;
+    }
+}
