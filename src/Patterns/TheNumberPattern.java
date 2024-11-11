c1d046ae04bfa9ab5113cc3870ac1d9e15709676
Aritra Chatterjee <ach142818@gmail.com>
2024-11-11 11:13:57 +0530

    the number pattern

--- src/Patterns/TheNumberPattern.java
+++ src/Patterns/TheNumberPattern.java
@@ -28,7 +28,7 @@ public class TheNumberPattern {
                 // Min of 4 directions and then we subtract from n
                 // because previously we would get a pattern whose border
                 // has 0's, but we want with border N's and then decrease inside.
-                System.out.print(n- Math.min(Math.min(top,bottom), Math.min(left,right)) + " ");
+                System.out.print(n - Math.min(Math.min(top,bottom), Math.min(left,right)) + " ");
             }
 
             // As soon as the numbers for each iteration are printed, we move to the
