9dc10b838e0362a1aa8cf08a138c3b37a49f8823
Aritra Chatterjee <ach142818@gmail.com>
2024-11-11 14:59:58 +0530

    Arrays in java

---
+++ src/DSA/Arrays/Arrays.java
@@ -0,0 +1,23 @@
+package DSA.Arrays;
+
+public class Arrays {
+    public static void main(String[] args) {
+
+        // Arrays
+
+        // Arrays are fundamental structures in Java that allow us to store multiple values of the same type in a single variable.
+        // They are useful for managing collections of data efficiently.
+
+        // Syntax
+        // data_type[] variable_name = new data_type[size];
+
+        // storing 5 numbers
+        int[] num = new int[5]; // Here int represents the type of data stored in the array
+
+        // Directly assigning the numbers in the array
+        int[] rollNo = {1, 2, 3, 4, 5}; // Here int represents the type of data stored in the array
+
+        int[] ros; // Declaration of array: ros is getting defined in the STACK
+        ros = new int[5]; // Initialization: Object is being created in the HEAP memory.
+    }
+}
