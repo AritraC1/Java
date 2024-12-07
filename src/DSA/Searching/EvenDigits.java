82260f890166e54c3eab13a7f1e4f693682b24e7
Aritra Chatterjee <ach142812@gmail.com>
2024-12-07 21:39:59 +0530

    Linear Search

--- src/DSA/Searching/EvenDigits.java
+++ src/DSA/Searching/EvenDigits.java
@@ -29,7 +29,17 @@ public class EvenDigits {
 
     // Function to count the number of digits
     static int countDigits(int num){
+        // if number is less than 0, i.e. negative then make it positive
+        if(num < 0) {
+            num = num * -1;
+        }
+
+        if (num == 0){
+            return 1;
+        }
+
         int count = 0;
+
         while(num>0){
             count++;
             num /= 10;
