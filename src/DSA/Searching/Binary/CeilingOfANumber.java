65b15050f660cb8523fb75e00842d6eb06833520
Aritra Chatterjee <ach142812@gmail.com>
2024-12-27 11:33:36 +0530

    Ceiling Of A Number

--- src/DSA/Searching/Binary/CeilingOfANumber.java
+++ src/DSA/Searching/Binary/CeilingOfANumber.java
@@ -38,6 +38,10 @@ public class CeilingOfANumber {
         int start = 0;
         int end = arr.length-1;
 
+        if(target > arr[arr.length - 1]){
+            return -1;
+        }
+
         while (start <= end){
             int mid = start + (end - start)/2;
 
