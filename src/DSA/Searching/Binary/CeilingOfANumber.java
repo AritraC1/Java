a016e1a3c482d1cec79d7bb018415fdfdcd720ad
Aritra Chatterjee <ach142812@gmail.com>
2024-12-27 11:25:25 +0530

    Ceiling Of A Number

--- src/DSA/Searching/Binary/CeilingOfANumber.java
+++ src/DSA/Searching/Binary/CeilingOfANumber.java
@@ -5,31 +5,50 @@ public class CeilingOfANumber {
         int[] arr = {2,3,5,9,14,16,18};
         int target = 15;
 
-        System.out.println(ceilingOfANumber(arr, target));
+        System.out.println(ceiling(arr, target));
     }
 
-    private static int ceilingOfANumber(int[] arr, int target) {
+//    private static int ceilingOfANumber(int[] arr, int target) {
+//        int start = 0;
+//        int end = arr.length-1;
+//        int ans = -1;
+//
+//        while (start <= end){
+//            int mid = start + (end - start)/2;
+//
+//            if (target < arr[mid]){
+//                ans = arr[mid];
+//                end = mid - 1;
+//            }
+//
+//            else if (target > arr[mid]){
+//                start = mid + 1;
+//            }
+//
+//            else {
+//               ans = arr[mid];
+//            }
+//        }
+//
+//        return ans;
+//    }
+
+    // More Better
+    private static int ceiling(int[] arr, int target) {
         int start = 0;
         int end = arr.length-1;
-        int ans = -1;
 
         while (start <= end){
             int mid = start + (end - start)/2;
 
             if (target < arr[mid]){
-                ans = arr[mid];
                 end = mid - 1;
-            }
-
-            else if (target > arr[mid]){
+            } else if (target > arr[mid]){
                 start = mid + 1;
-            }
-
-            else {
-               ans = arr[mid];
+            } else {
+                return mid;
             }
         }
-
-        return ans;
+        return start; // returns index of the answer
     }
 }
