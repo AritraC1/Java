2fff808a78437ecc948b09f8e03b0d01c8e31a3d
Aritra Chatterjee <ach142812@gmail.com>
2025-01-29 11:10:48 +0530

    Selection Sort

---
+++ src/DSA/Sorting/Insertion/InsertionSort.java
@@ -0,0 +1,19 @@
+package DSA.Sorting.Insertion;
+
+import java.util.Arrays;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        int[] arr = {3, 1, 5, 4, 2};
+
+        System.out.println("Original array: " + Arrays.toString(arr));
+        System.out.println("Sorted array: " + Arrays.toString(insertionSort(arr)));
+    }
+
+    private static int[] insertionSort(int[] arr) {
+
+        
+
+        return arr;
+    }
+}
