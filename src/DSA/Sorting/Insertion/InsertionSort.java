a0715e75fd379f1b0ba96e11eb38558eaa274784
Aritra Chatterjee <ach142812@gmail.com>
2025-02-06 14:19:21 +0530

    Insertion Sort added

--- src/DSA/Sorting/Insertion/InsertionSort.java
+++ src/DSA/Sorting/Insertion/InsertionSort.java
@@ -12,8 +12,22 @@ public class InsertionSort {
 
     private static int[] insertionSort(int[] arr) {
 
-        
+        for (int i = 0; i< arr.length-1; i++){
+            for (int j = i+1; j>0; j--) {
+                if (arr[j] < arr[j-1]) {
+                    swap(arr, j, j-1);
+                } else {
+                    break; // the left side of j is always sorted
+                }
+            }
+        }
 
         return arr;
+    }
+
+    static void swap(int[] arr, int a, int b) {
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
     }
 }
