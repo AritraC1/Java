9a85c649e3fc8c93784b2b16d0877bad37ea8e4b
Aritra Chatterjee <ach142812@gmail.com>
2025-02-17 09:50:52 +0530

    Cyclic Sort

---
+++ src/DSA/Sorting/Cycle/MissingNumber.java
@@ -0,0 +1,41 @@
+package DSA.Sorting.Cycle;
+
+import java.util.Arrays;
+
+public class MissingNumber {
+
+    public static void main(String[] args) {
+        int[] arr = {3,0,1};
+        System.out.println(missingNumber(arr));
+    }
+
+    public static int missingNumber(int[] arr) {
+        int i = 0;
+
+        // Cyclic Sort
+        while (i < arr.length) {
+            int correct = arr[i];
+            if (arr[i] < arr.length && arr[i] != arr[correct]) {
+                swap(arr, i , correct);
+            } else {
+                i++;
+            }
+        }
+
+        // search for first missing number
+        for (int index = 0; index < arr.length; index++) {
+            if (arr[index] != index) {
+                return index;
+            }
+        }
+
+        // case 2 (Edge Case)
+        return arr.length;
+    }
+
+    private static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
