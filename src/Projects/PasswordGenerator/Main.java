ca2ccc22e3b8bfb952ba7d697683d0ecd484163d
Aritra Chatterjee <ach142812@gmail.com>
2025-06-12 12:17:55 +0530

    New Project: Password Generator using java swing

---
+++ src/Projects/PasswordGenerator/Main.java
@@ -0,0 +1,12 @@
+package Projects.PasswordGenerator;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // instantiate the GUI
+        PasswordGeneratorGUI passwordGeneratorGUI = new PasswordGeneratorGUI();
+
+        // make the GUI visible
+        passwordGeneratorGUI.setVisible(true);
+    }
+}
